b91132e2b521d2566ca3117cb71e53366dbcb10e
y-mori-96 <y.mori2101@gmail.com>
2023-07-31 03:22:46 +0900

    レスポンシブ　ナビ表示中背景固定

--- js/components/hamburger.js
+++ js/components/hamburger.js
@@ -1,9 +1,11 @@
 function hamburger() {
   const hamburger = $(".hamburger");
   const nav = $(".company_nav");
+  const noScroll = $("#no_scroll");
 
   hamburger.click(function () {
     $(this).find(".hamburger_bar").toggleClass("active");
     nav.toggleClass("active");
+    noScroll.toggleClass("active");
   });
 }
